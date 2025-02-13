bab9ed9b46449ae720f76d06f033d6ed10c14a8a
FTC Neobots <neobots23252@gmail.com>
2025-02-13 22:16:49 +0100

    Breda coding is klaar, behalve servoposities

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/myOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/myOpMode.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -7,6 +8,7 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 
 @TeleOp(name = "myOpMode")
+@Disabled
 public class myOpMode extends LinearOpMode {
 
     private DcMotor leftFrontDrive;
